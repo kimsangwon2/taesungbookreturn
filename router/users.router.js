0216ed3d9e88a1d746f0a4db1ba1818462379ad3
ooommma <paosige@gmail.com>
2024-02-07 18:13:01 +0900

    프로필수정

--- router/users.router.js
+++ router/users.router.js
@@ -9,201 +9,284 @@ import { sendVerificationEmail } from "../middlewares/sendEmail.middlewares.js";
 const router = express.Router();
 
 router.post("/ad/sign-up", async (req, res) => {
-  try {
-    const { email, password, Checkpass, name } = req.body;
-    if (password.length < 6) {
-      return res.status(409).json({
-        message: "비밀번호가 6자 이상이어야 됩니다.",
-      });
-    }
-    if (password !== Checkpass) {
-      return res.status(409).json({
-        message: "비밀번호 확인과 일치해야 합니다.",
-      });
-    }
-    const isExistUser = await prisma.users.findFirst({
-      where: {
-        email,
-      },
-    });
+	try {
+		const { email, password, Checkpass, name } = req.body;
+		if (password.length < 6) {
+			return res.status(409).json({
+				message: "비밀번호가 6자 이상이어야 됩니다.",
+			});
+		}
+		if (password !== Checkpass) {
+			return res.status(409).json({
+				message: "비밀번호 확인과 일치해야 합니다.",
+			});
+		}
+		const isExistUser = await prisma.users.findFirst({
+			where: {
+				email,
+			},
+		});
 
-    if (isExistUser) {
-      return res.status(409).json({ message: "이미 존재하는 이메일입니다." });
-    }
+		if (isExistUser) {
+			return res
+				.status(409)
+				.json({ message: "이미 존재하는 이메일입니다." });
+		}
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-    const [user] = await prisma.$transaction(
-      async (tx) => {
-        const user = await tx.users.create({
-          data: {
-            email,
-            password: hashedPassword,
-            Checkpass: hashedPassword,
-            name,
-            permission: "Admin",
-            emailstatus: "yes",
-          },
-        });
-        return [user];
-      },
-      {
-        isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
-      }
-    );
+		const [user] = await prisma.$transaction(
+			async (tx) => {
+				const user = await tx.users.create({
+					data: {
+						email,
+						password: hashedPassword,
+						Checkpass: hashedPassword,
+						name,
+						permission: "Admin",
+						emailstatus: "yes",
+					},
+				});
+				return [user];
+			},
+			{
+				isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
+			}
+		);
 
-    return res.status(201).json({ message: "회원가입이 완료되었습니다." });
-  } catch (err) {
-    next(err);
-  }
+		return res.status(201).json({ message: "회원가입이 완료되었습니다." });
+	} catch (err) {
+		next(err);
+	}
 });
 
 router.post("/sign-up", async (req, res, next) => {
-  try {
-    const { email, password, Checkpass, name, emailstatus } = req.body;
-    if (password.length < 6) {
-      return res.status(409).json({
-        message: "비밀번호가 6자 이상이어야 됩니다.",
-      });
-    }
-    if (password !== Checkpass) {
-      return res.status(409).json({
-        message: "비밀번호 확인과 일치해야 합니다.",
-      });
-    }
-    const isExistUser = await prisma.users.findFirst({
-      where: {
-        email,
-      },
-    });
+	try {
+		const { email, password, Checkpass, name, emailstatus } = req.body;
+		if (password.length < 6) {
+			return res.status(409).json({
+				message: "비밀번호가 6자 이상이어야 됩니다.",
+			});
+		}
+		if (password !== Checkpass) {
+			return res.status(409).json({
+				message: "비밀번호 확인과 일치해야 합니다.",
+			});
+		}
+		const isExistUser = await prisma.users.findFirst({
+			where: {
+				email,
+			},
+		});
 
-    if (isExistUser) {
-      return res.status(409).json({ message: "이미 존재하는 이메일입니다." });
-    }
+		if (isExistUser) {
+			return res
+				.status(409)
+				.json({ message: "이미 존재하는 이메일입니다." });
+		}
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-    const [user] = await prisma.$transaction(
-      async (tx) => {
-        const token = Math.floor(Math.random() * 900000) + 100000;
-        const user = await tx.users.create({
-          data: {
-            email,
-            password: hashedPassword,
-            Checkpass: hashedPassword,
-            name,
-            emailstatus: "nono", // 상태를 '가입대기중'으로 설정
-            verificationToken: token.toString(), // token을 문자열로 변환하여 저장합니다.
-          },
-        });
+		const [user] = await prisma.$transaction(
+			async (tx) => {
+				const token = Math.floor(Math.random() * 900000) + 100000;
+				const user = await tx.users.create({
+					data: {
+						email,
+						password: hashedPassword,
+						Checkpass: hashedPassword,
+						name,
+						emailstatus: "nono", // 상태를 '가입대기중'으로 설정
+						verificationToken: token.toString(), // token을 문자열로 변환하여 저장합니다.
+					},
+				});
 
-        await sendVerificationEmail(email, token.toString()); // 난수 인증 코드를 이메일로 보냅니다.
-        return [user];
-      },
-      {
-        isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
-      }
-    );
-    const token = Math.floor(Math.random() * 900000) + 100000; // 6자리 숫자 난수 생성
-    return res.status(201).json({
-      message: "회원가입이 완료되었습니다. 이메일 인증 메일을 확인해주세요.",
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "회원가입에 실패했습니다." });
-  }
+				await sendVerificationEmail(email, token.toString()); // 난수 인증 코드를 이메일로 보냅니다.
+				return [user];
+			},
+			{
+				isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
+			}
+		);
+		const token = Math.floor(Math.random() * 900000) + 100000; // 6자리 숫자 난수 생성
+		return res.status(201).json({
+			message:
+				"회원가입이 완료되었습니다. 이메일 인증 메일을 확인해주세요.",
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: "회원가입에 실패했습니다." });
+	}
 });
 
 router.post("/sign-up/token", async (req, res, next) => {
-  try {
-    const { email, token } = req.body;
+	try {
+		const { email, token } = req.body;
 
-    const user = await prisma.users.findFirst({
-      where: { email },
-    });
+		const user = await prisma.users.findFirst({
+			where: { email },
+		});
 
-    if (!user) {
-      return res.status(404).json({ message: "유저를 찾을 수 없습니다." });
-    }
+		if (!user) {
+			return res
+				.status(404)
+				.json({ message: "유저를 찾을 수 없습니다." });
+		}
 
-    if (user.emailstatus !== "nono") {
-      return res
-        .status(409)
-        .json({ message: "이미 이메일 인증이 완료된 유저입니다." });
-    }
+		if (user.emailstatus !== "nono") {
+			return res
+				.status(409)
+				.json({ message: "이미 이메일 인증이 완료된 유저입니다." });
+		}
 
-    if (user.verificationToken !== token) {
-      return res
-        .status(400)
-        .json({ message: "인증 코드가 일치하지 않습니다." });
-    }
+		if (user.verificationToken !== token) {
+			return res
+				.status(400)
+				.json({ message: "인증 코드가 일치하지 않습니다." });
+		}
 
-    await prisma.users.update({
-      where: { userId: user.userId },
-      data: { emailstatus: "yes" },
-    });
+		await prisma.users.update({
+			where: { userId: user.userId },
+			data: { emailstatus: "yes" },
+		});
 
-    res.status(200).json({ message: "회원가입이 완료되었습니다." });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "서버 에러" });
-  }
+		res.status(200).json({ message: "회원가입이 완료되었습니다." });
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: "서버 에러" });
+	}
 });
 
 router.get("/oauth", (req, res) => {
-  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code`;
-  res.redirect(kakaoAuthUrl);
+	const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code`;
+	res.redirect(kakaoAuthUrl);
 });
 
 router.get("/oauth/callback", async (req, res) => {
-  const code = req.query.code;
-  const tokenRequest = await axios({
-    method: "POST",
-    url: "https://kauth.kakao.com/oauth/token",
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-    },
-    data: {
-      grant_type: "authorization_code",
-      client_id: process.env.KAKAO_ID,
-      redirect_uri: process.env.KAKAO_REDIRECT_URI,
-      code,
-    },
-  });
+	const code = req.query.code;
+	const tokenRequest = await axios({
+		method: "POST",
+		url: "https://kauth.kakao.com/oauth/token",
+		headers: {
+			"content-type": "application/x-www-form-urlencoded",
+		},
+		data: {
+			grant_type: "authorization_code",
+			client_id: process.env.KAKAO_ID,
+			redirect_uri: process.env.KAKAO_REDIRECT_URI,
+			code,
+		},
+	});
 
-  const { access_token } = tokenRequest.data;
-  const profileRequest = await axios({
-    method: "GET",
-    url: "https://kapi.kakao.com/v2/user/me",
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  });
+	const { access_token } = tokenRequest.data;
+	const profileRequest = await axios({
+		method: "GET",
+		url: "https://kapi.kakao.com/v2/user/me",
+		headers: {
+			Authorization: `Bearer ${access_token}`,
+		},
+	});
 
-  const { email, profile } = profileRequest.data.kakao_account;
-  const name = profile.nickname;
+	const { email, profile } = profileRequest.data.kakao_account;
+	const name = profile.nickname;
 
-  const users = await prisma.users.upsert({
-    where: { email },
-    update: { email, name },
-    create: {
-      email,
-      name,
-      password: "default",
-      Checkpass: "default",
-      emailstatus: "yes",
-    },
-  });
+	const users = await prisma.users.upsert({
+		where: { email },
+		update: { email, name },
+		create: {
+			email,
+			name,
+			password: "default",
+			Checkpass: "default",
+			emailstatus: "yes",
+		},
+	});
 
-  const userJWT = jwt.sign({ userId: users.id }, process.env.JWT_SECRET);
-  res.cookie("authorization", `Bearer ${userJWT}`);
+	const userJWT = jwt.sign({ userId: users.id }, process.env.JWT_SECRET);
+	res.cookie("authorization", `Bearer ${userJWT}`);
 
-  return res.status(200).json({ message: "로그인 성공" });
+	return res.status(200).json({ message: "로그인 성공" });
 });
 
 router.get("/oauth/logout", async (req, res) => {
-  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/logout?client_id=${process.env.KAKAO_ID}&logout_redirect_uri=${process.env.KAKAO_LOGOUT_URI}`;
-  res.redirect(kakaoAuthUrl);
+	const kakaoAuthUrl = `https://kauth.kakao.com/oauth/logout?client_id=${process.env.KAKAO_ID}&logout_redirect_uri=${process.env.KAKAO_LOGOUT_URI}`;
+	res.redirect(kakaoAuthUrl);
+});
+
+// 프로필 관리/ 수정 api
+router.put("user/:userId", authMiddleware, async (req, res) => {
+	const user = req.user;
+	const { userId } = req.params;
+	const { email, password, name, age } = req.body;
+
+	if (!userId) {
+		return res.status(400).json({
+			success: false,
+			message: "유저 아이디는 필수값입니다.",
+		});
+	}
+
+	if (!email) {
+		return res.status(400).json({
+			success: false,
+			message: "이메일은 필수값입니다.",
+		});
+	}
+
+	if (!password) {
+		return res.status(400).json({
+			success: false,
+			message: "비밀번호 필수값입니다.",
+		});
+	}
+
+	if (!name) {
+		return res.status(400).json({
+			success: false,
+			message: "이름은 필수값입니다.",
+		});
+	}
+
+	if (!age) {
+		return res.status(400).json({
+			success: false,
+			message: "나이는 필수값입니다.",
+		});
+	}
+
+	const profile = await prisma.users.findFirst({
+		where: {
+			userId: +userId,
+		},
+	});
+
+	if (!profile) {
+		return res.status(400).json({
+			success: false,
+			message: "존재하지 않는 유저 정보입니다",
+		});
+	}
+
+	if (profile.userId !== user.userId) {
+		return res.status(400).json({
+			success: false,
+			message: "올바르지 않은 요청입니다.",
+		});
+	}
+
+	await prisma.users.update({
+		where: {
+			userId: +userId,
+		},
+		data: {
+			email,
+			password,
+			name,
+			age,
+		},
+	});
+
+	return res.status(201).json({ message: "프로필 수정이 완료되었습니다." });
 });
 
 export default router;
